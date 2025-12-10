6f713e322f9c8298f4a82c04a6ce9405ebff08fa
João Pedro Nunes Aquino (mude depois) <167890111+Jao-Aqui@users.noreply.github.com>
2025-12-10 19:53:26 -0300

    Update Sequencial.c
    Adicionado include da STB lib

--- Sequencial.c
+++ Sequencial.c
@@ -1,7 +1,8 @@
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 
-
+#include "stb_image.h"
+#include "stb_image_write.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -122,3 +123,4 @@ int main() {
 
     return 0;
 }
+
